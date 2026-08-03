e6fafd6d3996a9f7373d17bb907b7a6d66b086e2
Pedro Urbano <pedroescapalete@gmail.com>
2026-08-03 09:41:55 -0300

    ft: Atualiza a Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,39 +1,42 @@
 import styles from './page.module.css';
 
-export default function Home() {
+export default () => {
     return (
         <main className={styles.container}>
             <div className={styles.content}>
-                <h1 className={styles.title}>Codeverse</h1>
-                <p className={styles.subtitle}>Explore. Aprenda. Colabore.</p>
+                <h1 className={styles.title}>Fundamentos do Next</h1>
+                <p className={styles.subtitle}>
+                    Organização e Praticas para desenvolvimento mais fluido
+                </p>
 
-                <a
-                    href="https://codeverse.dev.br"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={styles.cta}>
-                    <svg
-                        className={styles.ctaIcon}
-                        viewBox="-11.5 -10.23174 23 20.46348"
-                        width="25"
-                        height="25"
-                        aria-hidden="true">
-                        <circle r="2.05" fill="currentColor" />
-                        <g stroke="currentColor" strokeWidth="1" fill="none">
-                            <ellipse rx="11" ry="4.2" />
-                            <ellipse rx="11" ry="4.2" transform="rotate(60)" />
-                            <ellipse rx="11" ry="4.2" transform="rotate(120)" />
-                        </g>
-                    </svg>
-                    Ver material tutorial
-                </a>
+                <div className={styles.text}>
+                    <h2>Pasta src/app</h2>
+                    <p>
+                        No Next, a pasta src é usada para a organização da estrutura da aplicação.
+                        Podendo conter as páginas e seus layouts, dessa forma melhorando o
+                        desenvolvimento do projeto.
+                    </p>
 
-                <p className={styles.tagline}>Scaffolding oficial - Codeverse</p>
+                    <h2>Component Image</h2>
+                    <p>
+                        O componente Image do Next substitui a tag HTML, ele oferece mais
+                        performasse com otimizações já automatizadas.
+                    </p>
+
+                    <h2>CSS Module</h2>
+                    <p>
+                        O CSS Module é um tipo de estilização que apresenta um escopo local, dessa
+                        forma evitando conflitos com outras classes.
+                    </p>
+
+                    <h2>Componente Link</h2>
+                    <p>
+                        O componente Link é utilizado para realizar navegações entre várias páginas
+                        da aplicação. Ele demonstra transições mais rápidas e mantém a aplicação
+                        (SPA).
+                    </p>
+                </div>
             </div>
-
-            <footer className={styles.footer}>
-                <p className={styles.footerText}>Pouco pra quem vê..., muito pra quem aprende.</p>
-            </footer>
         </main>
     );
-}
+};
